e75094ba69919495aeff29d8afaaf9fbed82035d
deependra06112005-ai <deependra06112005@gmail.com>
2026-02-14 06:34:35 +0000

    basics day2

---
+++ basics01/prac2.js
@@ -0,0 +1,20 @@
+ /*let score = "44" 
+ console.log(typeof score)
+score = Number(score)
+console.log(typeof score)
+let score2 = "33abc"
+console.log(typeof score2)
+ /* Data type 
+ primitives: number, string, boolean, null, undefined, symbol 7 types of data types in js bigint
+ 
+ non-primitives / Refeerence dtat type : object, array, function
+memory stack (primitive)and heap(Non-Primitive data type)
+
+*/ 
+/*let myYoutubename = "deepu"
+
+let anotherName = myYoutubename
+anotherName = "deepu2"
+console.log(myYoutubename)
+console.log(anotherName)*/
+/* numbers nd maths
