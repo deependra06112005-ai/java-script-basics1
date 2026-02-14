4e276be995842b566f65cd1a110ff18cca11d274
deependra06112005-ai <deependra06112005@gmail.com>
2026-02-14 07:29:41 +0000

    4pillars

---
+++ basics01/DOM.js/dom.js
@@ -0,0 +1,13 @@
+//DOM Document Object Model
+//DOM is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
+//4 pillars of dom  
+//Selection of Element
+//changing in HTML
+//Changing in CSS
+//event listener 
+document.querySelector(".heading").innerText = "DOM Learning with JavaScript";
+document.querySelector(".heading").style.color = "white";
+document.querySelector(".heading").style.textAlign = "center";
+document.querySelector(".heading").style.padding = "20px";
+document.querySelector(".heading").style.fontSize = "40px";
+document.querySelector(".heading").style.fontFamily = "Arial, Helvetica, sans-serif";
